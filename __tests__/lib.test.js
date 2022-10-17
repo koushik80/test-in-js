6b7e377f59e4e7e7daa6ae9d2d65b6f6cf74231e
koushik <koushik.dey@live.com>
2022-10-17 13:41:14 +0300

    integration test

--- __tests__/lib.test.js
+++ __tests__/lib.test.js
@@ -1,4 +1,4 @@
-const { generateResult, validateInput } = require('../js/lib.js');
+const { generateResult, validateInput, checkAndGenerate } = require('../js/lib.js');
 
 // unit test for generateResult()
 test("testing generate result function", () => {
@@ -7,5 +7,12 @@ test("testing generate result function", () => {
 
 // unit test for validateInput()
 test("testing validate input function", () => {
-    expect(validateInput(1, true, true)). toBeTruthy();
+    expect(validateInput(1, true, true)).toBeTruthy();
+});
+
+// integration test for checkAndGenerate()
+
+test("testing check and generate function", async () => {
+    const result = await checkAndGenerate(1, "Title 1", "This is a test");
+    expect(result).toBe("User ID: 1 created an article titled Title 1");
 });
